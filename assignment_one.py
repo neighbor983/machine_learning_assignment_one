cdb882c206a558fc31e80ebcb542ff73d46fdc78
neighbor983 <kenneth.ray.baker@gmail.com>
2018-08-22 20:12:50 +0000

    intial input

---
+++ assignment_one.py
@@ -0,0 +1,54 @@
+'''
+Simple Linear Regression: You are given the following training data.
+X	Y
+2.0	5.1
+2.5	6.1
+3.0	6.9
+3.5	7.8
+4.0	9.2
+4.5	9.9
+5.0	11.5
+5.5	12.0
+6.0	12.8
+
+a.	Model Y as a linear function of X. 
+b.	Use gradient descent learning algorithm to learn model parameters.  Use an appropriate 
+    learning rate and convergence criterion. Plot J for the learning duration.
+'''
+
+training_data = [   {2.0, 5.1},
+                    {2.5,	6.1},
+                    {3.0,	6.9},
+                    {3.5,	7.8},
+                    {4.0,	9.2},
+                    {4.5,	9.9},
+                    {5.0,	11.5},
+                    {5.5,	12.0},
+                    {6.0,	12.8}];
+
+'Starting Values'
+theta_naught = 1;
+theta_one = 1;
+alpha = .5;
+
+
+
+'''
+Multiple Linear Regression
+You are given the following training data.  
+X1	X2	y
+0	1	0.6
+1	0	2.4
+1	1	1.6
+2	1	3.4
+1	2	0.5
+
+Model Y as a linear function of X1and X2.  Assume that the learning rate is 0.01, and initial values of the parameters are [1, 1, 1].  
+a.	Illustrate gradient descent algorithm by updating the parameters 3 iterations.  
+b.	Code and run the algorithm till convergence.
+You must submit the following
+1.	Source code
+2.	Output – Initial and final values of parameters; regression line and data; plots of J
+3.	Your observations and conclusions . 
+'''
+
