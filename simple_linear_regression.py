c9951e1352cc20b69f935ec64a797f43217d91d1
neighbor983 <kenneth.ray.baker@gmail.com>
2018-08-23 03:59:13 +0000

    renamed file to match problem name and changed function name for theta_naught

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -19,10 +19,11 @@ def Linear_Model(theta_0, theta_1, x_i):
     '''
     return theta_0 + theta_1 * x_i;
     
-def Mean_Squared_Error(theta_0, theta_1, dataList):
+def Theta0_Mean_Squared_Error(theta_0, theta_1, dataList):
     '''
     description: 
-        takes in the thetas and a dataList and produces the mean square error for a simple linear model
+        takes in the thetas and a dataList and produces the mean square error 
+        for theta_0 when using a simple linear model
     params:
         theta_0 = number
         theta_1 = number
@@ -47,7 +48,7 @@ def Simple_Linear_Theta0_Cost_Fuction(theta_0, theta_1, dataList):
         number
     '''
     m = len(dataList);
-    return Mean_Squared_Error(theta_0, theta_1, dataList) / 2.0;
+    return Theta0_Mean_Squared_Error(theta_0, theta_1, dataList) / 2.0;
     
 #Given Values
 training_data = [   { 'x': 2.0, 'y': 5.1},
@@ -66,5 +67,5 @@ theta_one = 1;
 learning_rate = .5;
 
 
-print(Mean_Squared_Error(theta_naught, theta_one, training_data));
+print(Theta0_Mean_Squared_Error(theta_naught, theta_one, training_data));
 print(Simple_Linear_Theta0_Cost_Fuction(theta_naught, theta_one, training_data));
